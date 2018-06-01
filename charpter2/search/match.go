ebfea42e61855bb434cb032772f2ab5050a25b35
ben <416854144@qq.com>
2018-06-01 23:16:02 +0800

    improve the logic in charpter 2

--- charpter2/search/match.go
+++ charpter2/search/match.go
@@ -12,11 +12,11 @@ type Result struct {
 type Matcher interface {
 	// not very clear about the parameter of function Search
 	// TODO
-	Search() ([]*Result, error)
+	Search(feed *Feed, searchTerm string) ([]*Result, error)
 }
 
-func Match(matcher Matcher, results chan *Result) {
-	searchResults, err := matcher.Search()
+func Match(matcher Matcher, feed *Feed, searchTerm string, results chan *Result) {
+	searchResults, err := matcher.Search(feed, searchTerm)
 
 	if err != nil {
 		log.Println(err)
@@ -27,5 +27,11 @@ func Match(matcher Matcher, results chan *Result) {
 	for _, result := range searchResults {
 		results <- result
 	
+	}
+}
+
+func Display(results chan *Result) {
+	for result := range results {
+		log.Printf("%s:\n%s\n\n", result.Field, result.Content)
 	}
 }
