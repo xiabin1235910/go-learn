049a1f6bc4f80d8f909bff2b1137888e477873bf
ben <416854144@qq.com>
2018-06-07 23:29:37 +0800

    add retrieve method of rss

--- charpter2/matcher/rss.go
+++ charpter2/matcher/rss.go
@@ -2,10 +2,38 @@ package matcher
 
 import (
 	"go-learn/charpter2/search"
+	"errors"
+	"net/http"
+	"encoding/xml"
+	"fmt"
+)
+
+type (
+	// todo 
+	rssDocument struct {}
 )
 
 type rssMatcher struct{}
 
-func (m rssMatcher) Retrieve(feed *search.Feed) {
+func (m rssMatcher) Retrieve(feed *search.Feed) (*rssDocument, error) {
+	if feed.URI == "" {
+		return nil, errors.New("URI cannot be empty")
+	}
+
+	res, err := http.Get(feed.URI)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("Http Response Error %d\n", res.StatusCode)
+	}
 	
+	defer res.Body.Close()
+	
+	var document rssDocument
+
+	err = xml.NewDecoder(res.Body).Decode(&document)
+	return &document, err
 }
