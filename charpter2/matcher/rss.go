a8ccb2af98ca930b1719e4e142d99949771b4def
ben <416854144@qq.com>
2018-06-08 14:52:45 +0800

    add rss matcher

--- charpter2/matcher/rss.go
+++ charpter2/matcher/rss.go
@@ -6,16 +6,102 @@ import (
 	"net/http"
 	"encoding/xml"
 	"fmt"
+	"regexp"
 )
 
 type (
-	// todo 
-	rssDocument struct {}
+	// item defines the fields associated with the item tag
+	// in the rss document.
+	item struct {
+		XMLName     xml.Name `xml:"item"`
+		PubDate     string   `xml:"pubDate"`
+		Title       string   `xml:"title"`
+		Description string   `xml:"description"`
+		Link        string   `xml:"link"`
+		GUID        string   `xml:"guid"`
+		GeoRssPoint string   `xml:"georss:point"`
+	}
+
+	// image defines the fields associated with the image tag
+	// in the rss document.
+	image struct {
+		XMLName xml.Name `xml:"image"`
+		URL     string   `xml:"url"`
+		Title   string   `xml:"title"`
+		Link    string   `xml:"link"`
+	}
+
+	// channel defines the fields associated with the channel tag
+	// in the rss document.
+	channel struct {
+		XMLName        xml.Name `xml:"channel"`
+		Title          string   `xml:"title"`
+		Description    string   `xml:"description"`
+		Link           string   `xml:"link"`
+		PubDate        string   `xml:"pubDate"`
+		LastBuildDate  string   `xml:"lastBuildDate"`
+		TTL            string   `xml:"ttl"`
+		Language       string   `xml:"language"`
+		ManagingEditor string   `xml:"managingEditor"`
+		WebMaster      string   `xml:"webMaster"`
+		Image          image    `xml:"image"`
+		Item           []item   `xml:"item"`
+	}
+
+	// rssDocument defines the fields associated with the rss document.
+	rssDocument struct {
+		XMLName xml.Name `xml:"rss"`
+		Channel channel  `xml:"channel"`
+	}
 )
 
 type rssMatcher struct{}
 
-func (m rssMatcher) Retrieve(feed *search.Feed) (*rssDocument, error) {
+func init() {
+	var matcher rssMatcher
+	search.Register("rss", matcher)
+}
+
+func (m rssMatcher) Search(feed *search.Feed, searchTerm string) ([]*search.Result, error) {
+	var results []*search.Result
+
+	rssDoc, err := m.retrieve(feed)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, channelItem := range rssDoc.Channel.Item {
+		matched, err := regexp.MatchString(searchTerm, channelItem.Title)
+
+		if err != nil {
+			return nil, err
+		}
+
+		if matched {
+			results = append(results, &search.Result{
+				Field: "Title",
+				Content: channelItem.Title,
+			})
+		}
+
+		matched, err := regexp.MatchString(searchTerm, channelItem.Description)
+		if err != nil {
+			return nil, err
+		}
+
+		if matched {
+			results = append(results, search.Result{
+				Field: "Description",
+				Content: channelItem.Description,
+			})
+		}
+
+		return results, nil
+	}
+	
+}
+
+func (m rssMatcher) retrieve(feed *search.Feed) (*rssDocument, error) {
 	if feed.URI == "" {
 		return nil, errors.New("URI cannot be empty")
 	}
